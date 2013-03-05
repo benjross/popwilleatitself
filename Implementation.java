013d23ca3a1938b0151e5eb516c92c7b142f0aa3
Jordan Hazari <hazarij@uw.edu>
2013-03-05 01:41:54 -0800

    Added comments to the interface

--- Implementation.java
+++ Implementation.java
@@ -1,6 +1,29 @@
 
 public interface Implementation {
+	
+	/**
+	 * Processes a query, and returns the population of the group and
+     * the percent of the total population.
+	 * 
+	 * @param west the west coordinate of the group.
+	 * @param south the south coordinate of the group.
+	 * @param east the east coordinate of the group.
+	 * @param north the north coordinate of the group.
+	 * @return the population of the group.
+	 */
     public int query(int west, int south, int east, int north);
+    
+    /**
+     * Parses the data file, and sets up the initial population
+     * grid.
+     * 
+     */
     public void preprocess();
+    
+    /**
+     * Gets the total population.
+     * 
+     * @return the total population.
+     */
     public int getPop();
 }
