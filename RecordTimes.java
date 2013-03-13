2fbd47c8dc25e1ecff65c7341702620baea2243d
Jordan Hazari <hazarij.uw.edu>
2013-03-12 20:40:57 -0700

    added experimentation file

--- RecordTimes.java
+++ RecordTimes.java
@@ -1,16 +1,69 @@
-
+/**
+ * Jordan Hazari(Primary author)
+ * Ben Ross
+ * CSE 332 AB
+ * Project 3 Experimentation
+ * 
+ * @author Jordan Hazari
+ * 
+ * This class performs experiments used for
+ * questions 6, 7, and 8 of the write-up.
+ *
+ */
 public class RecordTimes {
 
 	/**
-	 * @param args
+	 * @param args[0] the experiment to perform(6, 7, or 8).
+	 * @param args[1] the census data file to pe parsed.
 	 */
 	public static void main(String[] args) {
-		//q7();
-		q8();
+		if(args.length != 2) {
+			throw new IllegalArgumentException
+			("Please have an input argument of 6, 7, or 8, followed by a file name");
+		} else if (args[0].equals("6")) {
+			q6(args[1]);
+		} else if (args[0].equals("7")) {
+			q7(args[1]);
+		} else if (args[0].equals("7")) {
+			q8(args[1]);
+		} else {
+			throw new IllegalArgumentException
+			("Please have an input argument of 6, 7, or 8, followed by a file name");
+		}
 	}
 	
-	public static void q7() {
-		CensusData data = PopulationQuery.parse("CenPop.txt");
+	/**
+	 * Performs an experiment for question 6 of the
+	 * write-up, and outputs the data to the console.
+	 * 
+	 * @param file the census data file to be parsed.
+	 */
+	public static void q6(String file) {	  
+		CensusData data = PopulationQuery.parse(file);
+		PopulationQueryVerison imp2 = new SimpleAndParallel(100,500,data);
+		
+		for(int i = 10; i < data.data_size; i += 100) {
+			imp2.setCutoff(i);
+			double startTime = System.currentTimeMillis();
+			imp2.preprocess();
+			for(int j = 0; j < 100; j++) {
+				imp2.query(1, 1, 100, 500);
+			}
+			double endTime = System.currentTimeMillis();
+			
+			System.out.println(endTime-startTime);
+		}
+		
+	}
+	
+	/**
+	 * Performs an experiment for question 7 of the
+	 * write-up, and outputs the data to the console.
+	 * 
+	 * @param file the census data file to be parsed.
+	 */
+	public static void q7(String file) {
+		CensusData data = PopulationQuery.parse(file);
 		PopulationQueryVerison imp4;
 		PopulationQueryVerison imp5;
 		for(int i = 10; i < 200; i+=5) {
@@ -35,8 +88,14 @@ public class RecordTimes {
 		}
 	}
 	
-	public static void q8() {
-		CensusData data = PopulationQuery.parse("CenPop.txt");
+	/**
+	 * Performs an experiment for question 8 of the
+	 * write-up, and outputs the data to the console.
+	 * 
+	 * @param file the census data file to be parsed.
+	 */
+	public static void q8(String file) {
+		CensusData data = PopulationQuery.parse(file);
 		PopulationQueryVerison imp1 = new SimpleAndSequential(100,500,data);
 		PopulationQueryVerison imp2 = new SimpleAndParallel(100,500,data);
 		PopulationQueryVerison imp3 = new SmarterAndSequential(100,500,data);
@@ -51,28 +110,28 @@ public class RecordTimes {
 				double startTime = System.currentTimeMillis();
 				imp1.preprocess();
 				for (int k = 1; k < i; k++)
-					imp1.query(1, 1, 100, 500);
+					imp1.query(1, 1, 20, 25);
 				double endTime = System.currentTimeMillis();
 				time1 += (endTime - startTime);
 				
 				startTime = System.currentTimeMillis();
 				imp2.preprocess();
 				for (int k = 1; k < i; k++)
-					imp2.query(1, 1, 100, 500);
+					imp2.query(1, 1, 20, 25);
 				endTime = System.currentTimeMillis();
 				time2 += (endTime - startTime);
 				
 				startTime = System.currentTimeMillis();
 				imp3.preprocess();
 				for (int k = 1; k < i; k++)
-					imp3.query(1, 1, 100, 500);
+					imp3.query(1, 1, 20, 25);
 				endTime = System.currentTimeMillis();
 				time3 += (endTime - startTime);
 				
 				startTime = System.currentTimeMillis();
 				imp4.preprocess();
 				for (int k = 1; k < i; k++)
-					imp4.query(1, 1, 100, 500);
+					imp4.query(1, 1, 20, 25);
 				endTime = System.currentTimeMillis();
 				time4 += (endTime - startTime);
 			}
